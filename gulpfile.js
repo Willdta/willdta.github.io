fa41b8ca3456ce8a65e410a88be1b2f58f0c8ac7
Willdta <beyondutraining@gmail.com>
2018-05-14 14:35:43 -0400

    init commit / html layout

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+const stylus = require('gulp-stylus')
+const pug = require('gulp-pug')
+
+gulp.task('browserSync', ['stylus', 'pug', 'js'], () => {
+  browserSync.init({
+    server: {
+      baseDir: './',
+      index: './html/index.html'
+    }
+  })
+})
+
+gulp.task('stylus', () => {
+  return gulp.src('./stylus/*.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream())
+})
+
+gulp.task('pug', () => {
+  return gulp.src('./pug/*.pug')
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./html'))
+})
+
+gulp.task('js', () => {
+  return gulp.src('./js/*.js')
+    .pipe(gulp.dest('./js'))
+    .pipe(browserSync.stream())
+})
+
+gulp.task('watch', ['browserSync'], () => {
+  gulp.watch('./pug/*.pug', ['pug'])
+  gulp.watch('./stylus/*.styl', ['stylus'])
+  gulp.watch('./js/*.js', ['js'])
+  gulp.watch('./html/*.html').on('change', browserSync.reload)
+})
+
+gulp.task('default', ['stylus', 'pug', 'js', 'watch'])
