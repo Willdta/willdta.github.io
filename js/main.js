c2082bcd6ca0e0f384e62f799fbd7e0b108e6234
Willdta <beyondutraining@gmail.com>
2018-05-30 13:04:25 -0400

    added scrollToTop Link

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 $(() => {
-  const scrollLink = $('.menu')
+  $(window).scroll(function() {
+    const skillLink = document.getElementsByClassName('menu')[1].offsetTop + 100
+  
+    if (window.scrollY > skillLink) {
+      $('.landing-link').addClass('toggle-landing-link')
+    } else {
+      $('.landing-link').removeClass('toggle-landing-link')
+    }
+  })
 
-  scrollLink.click(function(e) {
+  $('.menu').click(function(e) {
     e.preventDefault()
 
     $('body, html').animate({
-      'scrollTop': $(this.hash).offset().top + 50
+      'scrollTop': $(this.hash).offset().top
     }, 1000)
   })
 })
