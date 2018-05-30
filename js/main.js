c5f17f4bb433273d9382967fa9ae0fdc03e2d122
Willdta <beyondutraining@gmail.com>
2018-05-29 23:25:56 -0400

    added smooth scrolling on anchor link click

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(() => {
+  const scrollLink = $('.menu')
+
+  scrollLink.click(function(e) {
+    e.preventDefault()
+
+    $('body, html').animate({
+      'scrollTop': $(this.hash).offset().top + 50
+    }, 1000)
+  })
+})
